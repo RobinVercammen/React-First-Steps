f4e79c3c439858d9b665dfcb64064b9652427d51
Robin Vercammen <robinvercammen1993@hotmail.com>
2016-10-12 20:54:51 +0200

    Fetch Api implemented

--- src/home.jsx
+++ src/home.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-export default class HomeComponent extends React.Component{
-    render(){
-        return (<h1>Hallo React</h1>);
+export default class HomeComponent extends React.Component {
+    componentWillMount() {
+        this.setState({ name: 'loading' });
+        fetch('http://pokeapi.co/api/v2/pokemon/1').then(data => {
+            data.json().then(
+                pokemon => {
+                    const name = pokemon.name;
+                    this.setState({ name });
+                }
+            );
+        });
+    }
+    render() {
+        return (<h1>{this.state.name}</h1>);
     }
 }
