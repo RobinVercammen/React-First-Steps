95d19d56404430598657dab0e2a4290c423b3d4d
Robin Vercammen <robinvercammen1993@hotmail.com>
2016-10-12 21:01:36 +0200

    fetch from input

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 
 export default class HomeComponent extends React.Component {
+    constructor() {
+        super();
+        this.updatePokemon = this.updatePokemon.bind(this);
+        this.updateState = this.updateState.bind(this);
+    }
+    updatePokemon(number) {
+        fetch(`http://pokeapi.co/api/v2/pokemon/${number}`).then(data => {
+            data.json().then(
+                pokemon => {
+                    const name = pokemon.name;
+                    this.setState({ name });
+                }
+            );
+        });
+    }
+    updateState(event) {
+        const number = event.target.value;
+        this.updatePokemon(number);
+    }
     componentWillMount() {
         this.setState({ name: 'loading' });
         fetch('http://pokeapi.co/api/v2/pokemon/1').then(data => {
@@ -13,6 +32,11 @@ export default class HomeComponent extends React.Component {
         });
     }
     render() {
-        return (<h1>{this.state.name}</h1>);
+        return (
+            <div>
+                <input type="number" min="1" max="750" onBlur={this.updateState} />
+                <h1>{this.state.name}</h1>
+            </div>
+        );
     }
 }
