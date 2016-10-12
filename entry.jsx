abdf44eb4c1e6fe1cae62cbb878c8b9209c81a6c
Robin Vercammen <robinvercammen1993@hotmail.com>
2016-10-12 20:35:44 +0200

    babel setup, jsx files build and run react

---
+++ entry.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import HomeComponent from './src/home';
+
+render(<HomeComponent />, document.getElementById('react-app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,33 @@
 module.exports = {
-    entry: "./entry.js",
+    entry: "./entry.jsx",
     output: {
         filename: "bundle.js"
     },
+    // where to find es6 modules
+    resolve: {
+        moduleDirectories: [
+            'node_modules',
+            'src'
+        ],
+        extensions: [
+            '',
+            '.js',
+            '.jsx'
+        ]
+    },
     module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: [
+                        'react',
+                        'es2015'
+                    ]
+                }
+            }
+        ]
     }
 };
